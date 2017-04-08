167339ed034c432dabeba90c8b74d6dd23aaac50
Dewald Laubscher <laubscher.dewald@gmail.com>
2017-04-08 12:17:39 +0200

    actually successfully poped a debug window

--- main.js
+++ main.js
@@ -1,11 +1,62 @@
+// Vars
+let waitForLazyLoader = [
+	'app',
+	'runtime',
+	'windows',
+];
+let ajax = new XMLHttpRequest();
+let response = undefined;
+
+// short handlers
+let defined = x => typeof x !== 'undefined' && x !== null;
+
+// Functions
+let loadFile = function (resolve) {
+	ajax.onreadystatechange = function() {
+		if (ajax.readyState == 4 && ajax.responseText !== "") {
+			response = JSON.parse(ajax.responseText);
+			resolve();
+		}
+	};
+	ajax.open('GET', 'file:///Users/dewald/node-debugger-url.json', true);
+	ajax.setRequestHeader("Content-type", "application/json");
+	ajax.send();
+}
+
+let checkLazyLoadAvailability = function (object) {
+	if (waitForLazyLoader.length === 0) return true;
+	let obCopy = object;
+	waitForLazyLoader[0].split('.').forEach(v => obCopy = obCopy[v]);
+	if (defined(obCopy)) {
+		waitForLazyLoader.splice(0, 1);
+		return checkLazyLoadAvailability(object);
+	}
+	return false;
+}
+
+let lazyLoadQueue = function (object, cb) {
+	checkLazyLoadAvailability(object);
+	if (waitForLazyLoader.length > 0) {
+		t = setTimeout(() => {
+			clearTimeout(t); // cleanup
+			lazyLoadQueue(object, cb);
+		}, 1000);
+	} else {
+		cb();
+	}
+}
+
 let boot = function () {
-	chrome.app.runtime.onLaunched.addListener(function() {
-		// chrome.app.window.create('chrome-devtools://devtools/remote/serve_file/@60cd6e859b9f557d2312f5bf532f6aec5f284980/inspector.html?experiments=true&v8only=true&ws=localhost:9229/5353f7ab-6bef-4e44-adb0-66bd5af17f74', {});
-		chrome.app.window.create('test.html', {
-		  id: 'main',
-		  bounds: { width: 620, height: 500 }
-		});
+	let test = chrome.windows.create({
+		url: response.url,
 	});
 }
 
-// boot();
+// Start
+new Promise(loadFile)
+	.then(() => {
+		lazyLoadQueue(chrome, () => {
+			boot();
+		})
+	});
+
